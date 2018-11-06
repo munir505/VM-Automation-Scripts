a01ea0c8bb06a005084705e15cf1dbfcdf32dbf4
Munir Kakar <munir.kakar@hotmail.com>
2018-11-06 11:43:37 +0000

    Adding automated scripts for jenkins and java/wget

---
+++ java_wget_script.bash
@@ -0,0 +1,2 @@
+sudo yum install -y java
+sudo yum install -y wget
---
+++ jenkins_script.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install -y jenkins
+sudo service jenkins start
+
