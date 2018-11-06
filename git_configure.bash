f49cc577e61995dde8b2a1f82a115e53ffe17bc3
Munir Kakar <munir.kakar@hotmail.com>
2018-11-06 12:00:40 +0000

    Updated Files

---
+++ git_configure.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config user.email "munir.kakar@hotmail.com"
+git config user.name "Munir Kakar"
+
--- install_script.bash
+++ install_script.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+./setup_script.bash
+./git_configuration.bash
 ./java_wget_script.bash
 ./jenkins_script.bash
 ./nexus_script.bash
