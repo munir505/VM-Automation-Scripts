e62d7bdd1676d734701a3667f636313dcf99dc42
Munir Kakar <munir.kakar@hotmail.com>
2018-11-14 14:53:43 +0000

    Install Terraform Script

---
+++ terraform.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum install wget -y
+sudo yum install unzip -y
+remote_zip="https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip"
+wget "${remote_zip}" -O "~/terraform.zip"
+sudo unzip ~/terraform.zip -d /opt/terraform/
+sudo ln -s /opt/terraform/terraform /usr/bin/terraform
