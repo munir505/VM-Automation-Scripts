e3d720c58bf110e27a4048c0967884eefa031b0f
munir505 <31385912+munir505@users.noreply.github.com>
2018-11-15 09:51:10 +0000

    Update jenkins_script.bash

--- jenkins_script.bash
+++ jenkins_script.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo yum install wget -y
+sudo yum install java -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install -y jenkins
